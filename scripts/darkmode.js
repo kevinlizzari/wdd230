a3561d95336c97a55079a701cff21ddd139e9386
kevinlizzari <liz22004@byui.edu>
2023-09-22 00:54:26 +0200

    Homa Page Enhancement

---
+++ scripts/darkmode.js
@@ -0,0 +1,23 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const cardSections = document.querySelectorAll(".card");
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("☑️")) {
+        main.style.backgroundColor = "#000";
+        main.style.color = "#fff";
+        cardSections.forEach(card => {
+            card.style.backgroundColor = "#111";
+            card.style.color = "#fff";
+        });
+        modeButton.textContent = "❎";
+    } else {
+        main.style.backgroundColor = "#eee";
+        main.style.color = "#000";
+        cardSections.forEach(card => {
+            card.style.backgroundColor = "#fff";
+            card.style.color = "#000";
+        });
+        modeButton.textContent = "☑️";
+    }
+});
