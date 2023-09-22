db7d7b35d51a5e9b29985b38b80a3ac052e2b7ff
kevinlizzari <liz22004@byui.edu>
2023-09-22 02:37:19 +0200

    darkmode upgrade

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,22 +1,30 @@
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
-const cardSections = document.querySelectorAll(".card");
-modeButton.addEventListener("click", () => {
-    if (modeButton.textContent.includes("☑️")) {
-        main.style.backgroundColor = "#000";
-        main.style.color = "#fff";
-        cardSections.forEach(card => {
-            card.style.backgroundColor = "#111";
-            card.style.color = "#fff";
-        });
-        modeButton.textContent = "❎";
+function toggleDarkMode() {
+    const main = document.querySelector("main");
+    const cardSections = document.querySelectorAll(".card");
+    //const modeButton = document.querySelector("#mode"); i keep this for testing one old error on the chrome console
+  
+    if (document.body.classList.contains("dark-mode")) {
+      document.body.classList.remove("dark-mode");
+      main.style.backgroundColor = "#eee";
+      main.style.color = "#000";
+      cardSections.forEach(card => {
+        card.style.backgroundColor = "#fff";
+        card.style.color = "#000";
+      });
+      document.querySelectorAll(".link").forEach(link => {
+        link.style.color = "blue"; 
+      });
     } else {
-        main.style.backgroundColor = "#eee";
-        main.style.color = "#000";
-        cardSections.forEach(card => {
-            card.style.backgroundColor = "#fff";
-            card.style.color = "#000";
-        });
-        modeButton.textContent = "☑️";
+      document.body.classList.add("dark-mode");
+      main.style.backgroundColor = "#000";
+      main.style.color = "#fff";
+      cardSections.forEach(card => {
+        card.style.backgroundColor = "#111";
+        card.style.color = "#fff";
+      });
+      document.querySelectorAll(".link").forEach(link => {
+        link.style.color = "#ff5733"; //  color in dark mode
+      });
     }
-});
+  }
+  
