c383ab64663d7481d328d0e3056353c9b432b669
kevinlizzari <liz22004@byui.edu>
2023-10-12 19:26:44 +0200

    testing

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,7 @@ const linksURL = "https://kevinlizzari.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    displayLinks(data.weeks);
   }
 
   function displayLinks(weeks) {
